dfe110eb38a285635019a3845295cfe8defee3db
x6eull <41849785+x6eull@users.noreply.github.com>
2025-01-17 22:04:43 +0800

    Initial commit

---
+++ android/app/src/main/java/com/zjuqsc/nx/MainActivity.java
@@ -0,0 +1,5 @@
+package com.zjuqsc.nx;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.zjuqsc.nx',
+  appName: 'mobile-nx',
+  webDir: 'dist'
+};
+
+export default config;
